9b59b6c17150d5ab952c9b7c41dd06982ec2246f
Sergey Mishin <sergey.mishin.71@gmail.com>
2026-05-28 16:07:57 +0300

    optimize images

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,19 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   trailingSlash: false,
   images: {
-    unoptimized: true,
+    unoptimized: false,
+    // Хранить кэш картинок в памяти/на диске не дольше 3 дней (в секундах)
+    minimumCacheTTL: 259200,
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'test.domfabrik.ru',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
